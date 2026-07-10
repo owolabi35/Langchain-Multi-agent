8fbacbde7e41ad816ec4748daf0881112eca0d77
Tajudeen Sanni <tjowo14@gmail.com>
2026-07-10 22:51:11 +0100

    tools created

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.tools.tools import web_search, scrape_url
+
+results = web_search.invoke("I need you to find a location at Barnawa Uganda street with house number 64 in Kaduna state Nigeria ")
+
+print(results)
